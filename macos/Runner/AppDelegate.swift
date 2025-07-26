aa2e9a4ea535c85c617bd1add56fcef04df9e8d1
Aaryan Karlapalem <aaryanlk@icloud.com>
2025-07-25 23:07:14 -0500

    Standardized app title and window size across all target platforms

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,6 +3,17 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    if let window = NSApplication.shared.windows.first {
+      let targetSize = NSSize(width: 1200, height: 800)
+      window.setContentSize(targetSize)
+      window.center()
+      window.minSize = NSSize(width: 800, height: 600)
+    }
+
+    super.applicationDidFinishLaunching(notification)
+  }
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
